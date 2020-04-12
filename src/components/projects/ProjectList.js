3ccd65e9f0e24143707eba21ee2858a2cbe1ca94
olivercastroteran <olivercastroteran@gmail.com>
2020-04-11 20:55:46 -0400

    Dashboard component added

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div className="project-ListeningStateChangedEvent section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Oli</p>
+          <p className="grey-text">april 10th, 2am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Oli</p>
+          <p className="grey-text">april 10th, 2am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Oli</p>
+          <p className="grey-text">april 10th, 2am</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
