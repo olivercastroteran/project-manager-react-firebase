bf629868e4efa844a8954522716c49a127640501
olivercastroteran <olivercastroteran@gmail.com>
2020-04-13 13:36:58 -0400

    Dashboard component connected to the store

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: '1', title: 'Create ciner page', content: 'bla bla bla' },
+    { id: '2', title: 'Upadate ciner page', content: 'blu blu blu' },
+    { id: '3', title: 'Create KO page', content: 'ble ble ble' },
+  ],
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
