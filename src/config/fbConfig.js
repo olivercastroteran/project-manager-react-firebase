1924deb1ecccad659d01238125be82e0d8a90486
olivercastroteran <olivercastroteran@gmail.com>
2020-04-13 18:43:04 -0400

    firebase installed & config file created

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyAW8SATKOiU9JLkWSlp_HjKBqzjxWKTeJo',
+  authDomain: 'project-manager-oct.firebaseapp.com',
+  databaseURL: 'https://project-manager-oct.firebaseio.com',
+  projectId: 'project-manager-oct',
+  storageBucket: 'project-manager-oct.appspot.com',
+  messagingSenderId: '444592983649',
+  appId: '1:444592983649:web:fc2f786416618f07a9a420',
+  measurementId: 'G-2TE0NKQNQD',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,6 +10,9 @@ const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
       console.log('created project', action.project);
+      break;
+    default:
+      console.log('idk');
   }
   return state;
 };
