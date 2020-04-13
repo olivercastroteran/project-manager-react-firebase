730ce97bea0bea612320791e6a8418c168bfe48c
olivercastroteran <olivercastroteran@gmail.com>
2020-04-12 23:18:52 -0400

    logo update and navbar style schanged

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import Logo from '../../img/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="nav-wrapper grey darken-3">
+    <nav className="nav-wrapper blue-grey">
       <div className="container">
         <Link to="/" className="brand-logo left">
-          ProjectMan
+          <img src={Logo} alt="projment" className="logo" />
         </Link>
         <SignedInLinks />
         <SignedOutLinks />
