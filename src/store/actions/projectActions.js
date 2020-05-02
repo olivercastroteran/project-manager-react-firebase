84d7df3012b1b93447b85f6cebc91246b06e4bb2
olivercastroteran <olivercastroteran@gmail.com>
2020-05-02 04:24:52 -0400

    create project functionality upgrated

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,14 +2,16 @@ export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to DB
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
 
     firestore
       .collection('projects')
       .add({
         ...project,
-        authorFirstName: 'Oliver',
-        authorLasttName: 'Castro Teran',
-        authorID: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorID: authorId,
         createdAt: new Date(),
       })
       .then(() => {
