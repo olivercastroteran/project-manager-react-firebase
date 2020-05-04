a450f3cbc6cc7d6b802ed119b0a6eb8c4f896ffe
olivercastroteran <olivercastroteran@gmail.com>
2020-05-03 20:13:15 -0400

    notification function on user signup added

--- functions/index.js
+++ functions/index.js
@@ -26,3 +26,20 @@ exports.projectCreated = functions.firestore
     };
     return createNotification(notification);
   });
+
+exports.userJoined = functions.auth.user().onCreate((user) => {
+  return admin
+    .firestore()
+    .collection('users')
+    .doc(user.uid)
+    .get()
+    .then((doc) => {
+      const newUser = doc.data();
+      const notification = {
+        content: 'Joined the project',
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp(),
+      };
+      return createNotification(notification);
+    });
+});
